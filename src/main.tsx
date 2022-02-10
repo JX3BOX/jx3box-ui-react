8658251ff7fa5069f430553334b08c3e139bd066
Harper Gao <harper.gao@ringcentral.com>
2022-02-10 15:06:36 +0800

    [JBR-18-LeftSidebar] Build React LeftSidebar

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,19 @@ import 'antd/dist/antd.css';
 import './vite.less';
 import Header from '@components/header';
 import Footer from '@components/footer';
+import LeftSidebar from '@components/sidebar/left-sidebar';
+
+const LeftSidebarContent = (props: any) => {
+  const { toggleLeftSidebar } = props;
+  return <span onClick={toggleLeftSidebar}>hello</span>;
+};
 
 const App = () => (
   <>
     <Header />
+    <LeftSidebar>
+      <LeftSidebarContent />
+    </LeftSidebar>
     <Footer />
   </>
 );
