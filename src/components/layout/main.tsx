df2a5d9cb61bf2927223316922c3cbcf85ac603f
Harper Gao <harper.gao@ringcentral.com>
2022-02-11 10:51:18 +0800

    [JBR-26-Layout-Main] Add main component

---
+++ src/components/layout/main.tsx
@@ -0,0 +1,28 @@
+import React, { useContext, useMemo } from 'react';
+import classNames from 'classnames';
+import { Jx3BoxLayoutContext } from '@components/provider/layout-provider';
+
+/**
+ *
+ * @param props
+ * @returns
+ */
+const LayoutMain: React.FC<{}> = props => {
+  const { leftSidebarVisible } = useContext(Jx3BoxLayoutContext);
+
+  /**
+   * 使用 useMemo 计算最终的 mainClassNames
+   * @param mainCls
+   */
+  const mainCls = useMemo(
+    () =>
+      classNames('c-main', {
+        ['without-left']: !leftSidebarVisible,
+      }),
+    [leftSidebarVisible]
+  );
+
+  return <main className={mainCls}>{props.children}</main>;
+};
+
+export default LayoutMain;
