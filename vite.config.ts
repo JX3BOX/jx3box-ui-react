c2e1031b014dfbedb1329d1cbc0bf53abdde5200
iRuxu <rx6@qq.com>
2022-02-18 05:25:59 +0800

    feat: optimize vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,87 +1,76 @@
-const path = require('path');
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svg from 'vite-plugin-react-svg';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: [
-      { find: '@components', replacement: '/src/components' },
-      { find: '@service', replacement: '/src/service' },
-      { find: '@data', replacement: '/src/data' },
-      { find: '@utils', replacement: '/src/utils' },
-    ],
-  },
 
-  css: {
-    preprocessorOptions: {
-      less: {
-        modifyVars: {
-          hack: `true; 
-          @import (reference) "${path.resolve('./node_modules/csslab/base.less')}"; 
-          @import (reference) "${path.resolve(
-            './node_modules/@jx3box/jx3box-common/css/var.less'
-          )}";
-          `,
+    // 🌸:alias @ for ./src
+    resolve: {
+        alias: [
+            { find: '@components', replacement: '/src/components' },
+            { find: '@service', replacement: '/src/service' },
+            { find: '@data', replacement: '/src/data' },
+            { find: '@utils', replacement: '/src/utils' },
+        ],
+    },
+
+    // ❄️:css mixins & global vars
+    css: {
+        preprocessorOptions: {
+            less: {
+                globalVars: {
+                    hack: `true; 
+                    @import "./node_modules/csslab/base.less";
+                    @import "./node_modules/@jx3box/jx3box-common/css/var.less";
+                    `
+                },
+            },
         },
-        javascriptEnabled: true,
-      },
     },
-  },
 
-  server: {
-    proxy: {
-      '/api/vip': {
-        target: 'https://pay.jx3box.com',
-        changeOrigin: true,
-      },
-      '/api/inspire': {
-        target: 'https://pay.jx3box.com',
-        changeOrigin: true,
-      },
-      '/api/team': {
-        target: 'http://gray.team.api.jx3box.com',
-        changeOrigin: true,
-        // "target": "https://team.api.jx3box.com",
-      },
-      '/api/cms': {
-        target: 'http://cms.jx3box.com',
-        changeOrigin: true,
-      },
-      '/api/messages': {
-        target: 'https://helper.jx3box.com',
-        changeOrigin: true,
-      },
-      '/api/post/favorite': {
-        target: 'https://helper.jx3box.com',
-        changeOrigin: true,
-      },
-      '/api/wiki': {
-        target: 'https://helper.jx3box.com',
-        changeOrigin: true,
-      },
-      '/api': {
-        target: 'https://next.jx3box.com',
-        changeOrigin: true,
-      },
+    // 🌈:cross-origin
+    server: {
+        proxy: {
+            '/api/vip': {
+                target: 'https://pay.jx3box.com',
+                changeOrigin: true,
+                // 前端请求路径不变，用于后端灰度测试替换为测试路径
+                // rewrite: (path) => path.replace(/^\/api/, ""),
+                // configure: (proxy, options) => {
+                // proxy 是 'http-proxy' 的实例
+                // }
+            },
+            '/api/messages': {
+                target: 'https://helper.jx3box.com',
+                changeOrigin: true,
+            },
+            '/api/cms': {
+                target: 'https://cms.jx3box.com',
+                changeOrigin: true,
+            },
+            '/api': {
+                target: 'https://next2.jx3box.com',
+                changeOrigin: true,
+            },
+        },
     },
-  },
 
-  plugins: [
-    react(),
+    // 🍬:loaders
+    plugins: [
+        react(),
 
-    /**
-     * Add rollup inline react svg plugin
-     * @plugin rollup-plugin-react-svg
-     */
-    svg({
-      defaultExport: 'component',
-      svgo: true,
-      memo: true,
-      svgoConfig: {
-        removeViewBox: false,
-      },
-    }),
-  ],
+        /**
+         * Add rollup inline react svg plugin
+         * @plugin rollup-plugin-react-svg
+         */
+        // svg({
+        //     defaultExport: 'component',
+        //     svgo: true,
+        //     memo: true,
+        //     svgoConfig: {
+        //         removeViewBox: false,
+        //     },
+        // }),
+    ],
 });
